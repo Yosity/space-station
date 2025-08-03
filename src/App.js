9f291bd1174196067971b3139dfbb851ff82f162
yosefurr <yosefanwar690@gmail.com>
2025-08-03 21:15:46 +0300

    minor change

--- src/App.js
+++ src/App.js
@@ -90,6 +90,7 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/destination" element={<Destination />} />
         <Route path="/crew" element={<Crew />} />
         <Route path="/technology" element={<Technology />} />
